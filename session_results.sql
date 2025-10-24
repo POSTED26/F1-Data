d343808e33fc938da8102a993bb8f6d625cc686c
tilbo26 <tilbo2@gmail.com>
2025-10-24 15:48:02 -0700

    cleaned and organized project folder

--- session_results.sql
+++
@@ -1,7 +0,0 @@
--- [['session_key', 'driver_number', 'position']]
-CREATE TABLE IF NOT EXISTS session_results (
-    session_key Integer,
-    driver_number Integer,
-    position Integer,
-    PRIMARY KEY(session_key, driver_number)
-);
